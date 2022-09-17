28e147ba9366c74cce2b446c315f8779a0d21828
QasidSaleem <qasidsaleem@gmail.com>
2022-09-17 22:48:35 +0200

    added lightning model module

---
+++ lit_models/lit_model.py
@@ -0,0 +1,85 @@
+"""Lightning Model."""
+import pytorch_lightning as pl
+import torch
+OPTIMIZER = "Adam"
+LR = 1e-3
+LOSS = "MSELoss"
+ONE_CYCLE_TOTAL_STEPS = 100
+
+class LitModule(pl.LightningModule):
+    """
+    Generic PyTorch-Lightning class that must be initialized with a PyTorch module.
+    """
+    def __init__(self, model, args):
+        super().__init__()
+        self.model = model
+        self.args = args
+
+        optimizer = self.args.get("optimizer", OPTIMIZER)
+        self.optimizer_class = getattr(torch.optim, optimizer)
+
+        self.lr = self.args.get("lr", LR)
+
+        loss = self.args.get("loss", LOSS)
+        self.loss_fn = getattr(torch.nn, loss)
+
+        self.one_cycle_max_lr = self.args.get("one_cycle_max_lr", None)
+        self.one_cycle_total_steps = self.args.get("one_cycle_total_steps", ONE_CYCLE_TOTAL_STEPS)
+    
+    def configure_optimizers(self):
+        optimizer = self.optimizer_class(self.parameters(), lr=self.lr)
+        if self.one_cycle_max_lr is None:
+            return optimizer
+        scheduler = torch.optim.lr_scheduler.OneCycleLR(
+            optimizer=optimizer, max_lr=self.one_cycle_max_lr, total_steps=self.one_cycle_total_steps
+        )
+        return {"optimizer": optimizer, "lr_scheduler": scheduler}
+    
+    def forward(self, x):
+        return self.model(x)
+    
+    def predict(self, x):
+        return self.model(x)
+    
+    def _run_on_batch(self, batch):
+        pred = self(batch)
+        y = batch[:, self.model.n_seeds:, :]
+        loss = self.loss_fn(pred, y)
+        
+        return pred, y, loss
+    
+    def training_step(self, batch, batch_idx):
+        pred, y, loss = self._run_on_batch(batch)
+        # self.log("train_loss", loss, batch_size=pred.shape[0], on_step=True, on_epoch=True)
+        logs = {}
+        logs["loss"] = loss
+
+        for k,v in logs.items():
+            self.log(f"train_{k}", v, batch_size=pred.shape[0], on_step=True, on_epoch=True, prog_bar=True)
+        
+        return loss
+    
+    def validation_step(self, batch, batch_idx):
+        pred, y, loss = self._run_on_batch(batch)
+        logs = {}
+        logs["loss"] = loss
+        for k,v in logs.items():
+            prog_bar = True if k == "loss" else False
+            self.log(f"val_{k}", v, batch_size=pred.shape[0], on_step=False, on_epoch=True, prog_bar=prog_bar)
+        
+        return loss
+    
+    def test_step(self, batch, batch_idx):
+        pred, y, loss = self._run_on_batch(batch)
+        logs = {}
+        logs["loss"] = loss
+        for k,v in logs.items():
+            self.log(f"test_{k}", v, batch_size=pred.shape[0], on_step=False, on_epoch=True, prog_bar=False)
+        
+        return loss
+
+
+
+
+
+
